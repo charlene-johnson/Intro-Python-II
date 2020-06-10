7e1524cb6fdccd24e6e73c030b5cca2191952e0e
Danielle Baxter <danibaxx@hotmail.com>
2020-06-09 18:40:53 -0600

    init commit, added player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+      self.name = name
+      self.current_room = current_room
+      self.move_room = []
+
+    def __str__(self):
+      return f'Welcome {self.name}! You are currently in the {self.current_room}'
+
+player1 = Player('Dulfy the Great', 'Foyer')
+
+print(player1)
+
