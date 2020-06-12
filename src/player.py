7ef08a025276cdf38156817a7c51db26c01fdb2d
Danielle Baxter <danibaxx@hotmail.com>
2020-06-11 18:19:29 -0600

    extracted code to adv.py

--- src/player.py
+++ src/player.py
@@ -1,15 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room=None):
       self.name = name
       self.current_room = current_room
-      self.move_room = []
 
-    def __str__(self):
-      return f'Welcome {self.name}! You are currently in the {self.current_room}'
-
-player1 = Player('Dulfy the Great', 'Foyer')
-
-print(player1)
 
