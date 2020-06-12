4f03888153eaf42a848ab5ac2b7c7287647145dc
Danielle Baxter <danibaxx@hotmail.com>
2020-06-11 18:19:42 -0600

    added in directions

--- src/room.py
+++ src/room.py
@@ -4,6 +4,20 @@ class Room:
   def __init__(self, name, description):
     self.name = name
     self.description = description
+    self.n_to = None
+    self.s_to = None
+    self.e_to = None
+    self.w_to = None
 
   def __str__(self):
-      return f'{self.name}: {self.description}'
+      output = f'{self.name}: {self.description}\n'
+      if self.n_to:
+        output += 'To the North is: ' + self.n_to.name + '\n'
+      if self.s_to:
+        output += 'To the South is: ' + self.s_to.name + '\n'
+      if self.e_to:
+        output += 'To the East is: ' + self.e_to.name + '\n'
+      if self.w_to:
+        output += 'To the West is: ' + self.w_to.name + '\n'
+
+      return output
