6ef1c19ba1ac5b92add9b97f2ef70fbd377e6924
Danielle Baxter <danibaxx@hotmail.com>
2020-06-11 20:20:34 -0600

    removed some code for functionality

--- src/adv.py
+++ src/adv.py
@@ -1,21 +1,21 @@
-
+from room import Room
+from player import Player
 
 # Declare all the s
 # dictionaries of s
-# can get rid of the dict  = {} and make each into variables
-'outside' =  ("Outside Cave Entrance", "North of you, the cave mount beckons")
+outside =  Room("Outside Cave Entrance", "North of you, the cave mount beckons")
 
-'foyer' = ("Foyer", """Dim light filters in from the south. Dusty
+foyer = Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""")
 
-'overlook' = ("Grand Overlook", """A steep cliff appears before you, falling
+overlook = Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""")
 
-'narrow' = ("Narrow Passage", """The narrow passage bends here from west
+narrow = Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""")
 
-'treasure' = ("Treasure Chamber", """You've found the long-lost treasure
+treasure = Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""")
 
@@ -33,30 +33,48 @@ narrow.w_to = foyer
 narrow.n_to = treasure
 treasure.s_to = narrow
 
-#
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' .
-player = Player('Dulfy the Great',)
-player.current_ = outside
-print(player.current_)
+player = Player('Dulfy the Great')
+player.current_room = outside
+print(player.name)
+
 # Write a loop that:
 while True:
-# * Prints the current  name
+# * Prints the current room name
+    print(player.current_room.name)
 # * Prints the current description (the textwrap module might be useful here).
+    print(f'{player.current_room.description}')
 # * Waits for user input and decides what to do.
-#
+    player_input = input('Choose a direction :' + '\n')
 # If the user enters a cardinal direction, attempt to move to the  there.
-    if variable == 'n':
-        # check if the current  has a n_to attribute
-        if hasattr(player.current_, f'{variable}_to'):
-        # or can do 4 if statements if player.current_.n_to is not None:
-        # 2-4 ifs need to be elif
-        # add other if statements to handle wrong directions
-            # move player to 
-            player.current_ = getattr(player.current_ f'{variable}_to')
-            # or can do 4 statements of player.current_ = player.current_.n_to
+    
+    if player_input == 'n':
+        # check if the current room has a n_to attribute
+        if player.current_room.n_to is not None:
+            player.current_room = player.current_room.n_to
+
+    elif player_input == 's':
+        if player.current_room.s_to is not None:
+            player.current_room = player.current_room.s_to
+
+    elif player_input == 'e':
+        if player.current_room.e_to is not None:
+            player.current_room = player.current_room.e_to
+
+    elif player_input == 'w':
+        if player.current_room.w_to is not None:
+            player.current_room = player.current_room.w_to
+
+    elif player_input == 'q':
+        break
+
+    else: # player_input not in ('n', 's', 'w', 'e'):
+        print('Incorrect input, try another direction')
+
+
+
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
